aa2d069552cf65fbf162ab3fa35c0e9d373c8858
Francesco Vo <vofrancesco99@gmail.com>
2023-06-26 16:39:47 +0200

    review presentation

--- igraph.m
+++ igraph.m
@@ -2,13 +2,12 @@ close all
 clear variables
 clc
 
-M = [0 1 0 1 1;
-     1 0 1 0 0;
-     0 1 0 1 0;
-     1 0 1 0 0;
-     1 0 0 0 0];
+M = [0 1 1 1;
+    1 0 0 1;
+    1 0 0 0;
+    1 1 0 0];
 
 G = graph(M);
 p = plot(G);
-labelnode(p, [1 2 3 4 5], {'Age', 'RestingBP', 'Cholesterol', 'MaxHR', 'Oldpeak'})
+labelnode(p, [1 2 3 4], {'Age', 'RestingBP', 'MaxHR', 'Oldpeak'})
 saveas(gcf,'igraph.png')
